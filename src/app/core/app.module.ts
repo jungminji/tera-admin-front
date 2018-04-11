40ea093b5b3120c7f96c8740cd13b551548094e8
jungminji <jungminji@jungminjis-MacBook-Pro.local>
2018-04-11 16:44:21 +0900

    attaching ngrx/store

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 import { AppComponent } from './components/app.component';
 import { AppRouteModule } from './app-route.module';
 import { ToolbarModule } from '../views/toolbar/toolbar.module';
 import { SidebarModule } from '../views/sidebar/sidebar.module';
 
+import { environment } from '../../environments/environment';
+
+export const metaReducers: MetaReducer<any>[] = !environment.production ? [storeFreeze] : [];
 
 @NgModule({
   declarations: [
@@ -16,7 +22,10 @@ import { SidebarModule } from '../views/sidebar/sidebar.module';
     BrowserModule,
     AppRouteModule,
     ToolbarModule,
-    SidebarModule
+    SidebarModule,
+    StoreModule.forRoot({}, { metaReducers }),
+    EffectsModule.forRoot([]),
+    environment.development ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
